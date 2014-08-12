351b9eeb4f912f77f94812cbf2dafd671cc589f8
Joe Knapp <jmknapp@gmail.com>
2014-08-12 05:25:31 -0400

    server connectivity test; added mslalert wav file

--- www/js/mslmap.js
+++ www/js/mslmap.js
@@ -78,6 +78,27 @@ function driveinfotoast() {
     };
 }
 
+// check server connectivity
+function testserver() {
+    var xhr = new XMLHttpRequest();
+    var file = "http://curiosityrover.com/tracking/ajax/test.html";
+    var randomNum = Math.round(Math.random() * 10000);
+
+    xhr.open('HEAD', file + "?rand=" + randomNum, false);
+
+    try {
+        xhr.send();
+
+        if (xhr.status >= 200 && xhr.status < 304) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
+}
+
 // load overall track
 function gettrack() {
   traverse = new Array() ;
